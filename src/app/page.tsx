23d3c5f86f830cea34c753b93c6bd67755b0dc4c
zweird <zweir.dd@gmail.com>
2025-09-17 09:37:58 +0200

    refactor(auth): protect auth routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import CreateGameForm from "@/components/forms/create-game-form"
 import CenteredDiv from "@/components/ui/centered-div"
 
 const Home = () => (
   <CenteredDiv>
-    <CreateGameForm />
+    <p>hello</p>
   </CenteredDiv>
 )
 
