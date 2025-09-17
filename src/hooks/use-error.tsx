e3b5d3c9e5bd8ccbe7b11efafa1deb0f1e991619
zweird <zweir.dd@gmail.com>
2025-09-17 09:44:38 +0200

    fix: typecheck

--- src/hooks/use-error.tsx
+++ src/hooks/use-error.tsx
@@ -2,9 +2,9 @@ import { useTranslations } from "next-intl"
 import { toast } from "sonner"
 
 import type { ApiClientError } from "@/api/errors"
-import type { ErrorCode } from "@/types/api"
+import type { AuthErrorCode, ErrorCode } from "@/types/api"
 
-const GLOBAL_ERRORS_CODES: ErrorCode[] = [
+const GLOBAL_ERRORS_CODES: (ErrorCode | AuthErrorCode)[] = [
   "SOMETHING_WENT_WRONG",
   "FORBIDDEN",
   "UNAUTHORIZED",
