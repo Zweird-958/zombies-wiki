f805dec29cbf217bf1262c353a8984111c837b23
julien-hamel <julien@tulyp.io>
2025-09-24 16:55:53 +0200

    refactor(input): add ring color

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -9,7 +9,7 @@ export type InputProps = OmitClassName<ComponentProps<typeof BaseInput>>
 export const Input = ({ className, ...props }: InputProps) => (
   <BaseInput
     className={cn(
-      "border-input rounded-md border px-2 py-1 focus:ring focus:outline-none",
+      "border-input ring-ring rounded-md border px-2 py-1 focus:ring focus:outline-none",
       className,
     )}
     {...props}
