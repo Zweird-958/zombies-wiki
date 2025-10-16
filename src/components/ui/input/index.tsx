d7cb76ad229d7e10cd94c055ee7d3c7af17234d2
zweird <zweir.dd@gmail.com>
2025-10-16 15:06:21 +0200

    refactor(input): add change cursor and opacity on disabled

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,15 +1,10 @@
 import { Input as BaseInput } from "@base-ui-components/react/input"
 
+import { input } from "@/components/ui/input/variants"
 import { cn } from "@/utils/cn"
 
 export type InputProps = BaseInput.Props
 
 export const Input = ({ className, ...props }: InputProps) => (
-  <BaseInput
-    className={cn(
-      "border-input rounded-md border px-2 py-1 focus:ring focus:outline-none",
-      className,
-    )}
-    {...props}
-  />
+  <BaseInput className={cn(input(), className)} {...props} />
 )
