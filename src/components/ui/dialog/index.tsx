d72adec0a259f2a22ee252522dd1f202fb4024a7
zweird <zweir.dd@gmail.com>
2025-10-16 15:04:22 +0200

    refactor(dialog): add all className on variant file

--- src/components/ui/dialog/index.tsx
+++ src/components/ui/dialog/index.tsx
@@ -2,7 +2,10 @@ import { Dialog as BaseDialog } from "@base-ui-components/react/dialog"
 import { X } from "lucide-react"
 import type { ComponentProps } from "react"
 
+import { dialog } from "@/components/ui/dialog/variants"
 import { cn } from "@/utils/cn"
+
+const styles = dialog()
 
 export type DialogProps = BaseDialog.Root.Props
 
@@ -27,22 +30,16 @@ export const DialogPopup = ({
   <BaseDialog.Portal>
     <BaseDialog.Backdrop
       data-slot="dialog-backdrop"
-      className="starting-style:opacity-0 ending-style:opacity-0 fixed inset-0 bg-black opacity-70 transition-all duration-150"
+      className={styles.backdrop()}
     />
     <BaseDialog.Popup
       data-slot="dialog-popup"
-      className={cn(
-        "bg-popover text-popover-foreground starting-style:scale-90 starting-style:opacity-0 ending-style:scale-90 ending-style:opacity-0 border-border fixed top-1/2 left-1/2 z-50 max-w-[calc(100vw-3rem)] min-w-96 -translate-x-1/2 -translate-y-1/2 rounded-lg border p-6 transition-all duration-150",
-        className,
-      )}
+      className={cn(styles.popup(), className)}
       {...props}
     >
       {children}
 
-      <BaseDialog.Close
-        data-slot="dialog-close"
-        className="svg-not-size:size-4 svg:pointer-events-none svg:shrink-0 absolute top-4 right-4 rounded-full opacity-70 outline-0 transition-opacity hover:opacity-100 disabled:pointer-events-none"
-      >
+      <BaseDialog.Close data-slot="dialog-close" className={styles.close()}>
         <X />
         <span className="sr-only">{closeText}</span>
       </BaseDialog.Close>
@@ -55,7 +52,7 @@ export type DialogContentProps = ComponentProps<"div">
 export const DialogContent = ({ className, ...props }: DialogContentProps) => (
   <div
     data-slot="dialog-content"
-    className={cn("pt-2", className)}
+    className={cn(styles.content(), className)}
     {...props}
   />
 )
@@ -65,7 +62,7 @@ export type DialogTitleProps = BaseDialog.Title.Props
 export const DialogTitle = ({ className, ...props }: DialogTitleProps) => (
   <BaseDialog.Title
     data-slot="dialog-title"
-    className={cn("text-lg font-medium", className)}
+    className={cn(styles.title(), className)}
     {...props}
   />
 )
@@ -78,7 +75,7 @@ export const DialogDescription = ({
 }: DialogDescriptionProps) => (
   <BaseDialog.Description
     data-slot="dialog-description"
-    className={cn("text-muted-foreground text-sm", className)}
+    className={cn(styles.description(), className)}
     {...props}
   />
 )
