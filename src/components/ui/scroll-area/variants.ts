d1f7251f4e0f60b2a17e881c4bb3a4459b725afb
zweird <zweir.dd@gmail.com>
2025-10-17 11:15:16 +0200

    refactor(scrollbar): add horizontal margin

--- src/components/ui/scroll-area/variants.ts
+++ src/components/ui/scroll-area/variants.ts
@@ -6,7 +6,7 @@ export const scrollArea = tv({
     viewport: "border-border h-full overscroll-contain rounded-md border",
     content: "px-4 py-3",
     scrollbar:
-      "bg-muted relative my-1 flex w-1 justify-center rounded-md opacity-0 transition-all group-hover:opacity-100",
+      "bg-muted relative m-1 flex w-1 justify-center rounded-md opacity-0 transition-all group-hover:opacity-100",
     thumb: "bg-muted-foreground w-full rounded-md",
   },
 })
