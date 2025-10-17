d5e4ff385e9b3a7f47c5606ec4d1e3ead7a904c6
zweird <zweir.dd@gmail.com>
2025-10-17 11:26:45 +0200

    refactor(toggle): add disabled styles

--- src/components/ui/toggle/variants.ts
+++ src/components/ui/toggle/variants.ts
@@ -1,12 +1,22 @@
 import { type VariantProps, tv } from "tailwind-variants"
 
 export const toggle = tv({
-  base: "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium hover:bg-muted hover:text-muted-foreground disabled:pointer-events-none disabled:opacity-50 pressed:bg-accent pressed:text-accent-foreground [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] outline-none transition-[color,box-shadow] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive whitespace-nowrap",
+  base: [
+    "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap",
+    "transition-[color,box-shadow]",
+    "pressed:bg-accent pressed:text-accent-foreground",
+    "svg:pointer-events-none svg-not-size:size-4 svg:shrink-0",
+    "focused:ring outline-none",
+    "disabled:cursor-not-allowed disabled:opacity-disabled",
+  ],
   variants: {
     variant: {
-      default: "bg-transparent",
-      outline:
-        "border border-input bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground",
+      default:
+        "bg-transparent hover:not-disabled:bg-muted hover:not-disabled:text-muted-foreground",
+      outline: [
+        "border border-input bg-transparent shadow-xs",
+        "hover:not-disabled:bg-accent hover:not-disabled:text-accent-foreground",
+      ],
     },
     size: {
       default: "h-9 px-2 min-w-9",
