bacb723c6421c684a2948b6c7881b2b5a5db911f
zweird <zweir.dd@gmail.com>
2025-10-17 11:42:07 +0200

    refactor(toolbar): add disabled

--- src/components/ui/toolbar/variants.ts
+++ src/components/ui/toolbar/variants.ts
@@ -2,9 +2,13 @@ import { tv } from "tailwind-variants"
 
 export const toolbar = tv({
   slots: {
-    base: "border-border flex w-fit gap-1 rounded-md border p-0.5",
+    base: [
+      "border-border flex w-fit gap-1 rounded-md border p-0.5",
+      "disabled:opacity-disabled disabled:cursor-not-allowed disabled:**:cursor-not-allowed",
+    ],
     group: "flex gap-0.5",
-    button: "hover:bg-accent hover:text-accent-foreground rounded-md px-2",
+    button:
+      "hover:not-disabled:bg-accent hover:not-disabled:text-accent-foreground rounded-md px-2",
     separator: "bg-border my-1 min-h-full w-[1px] rounded-md",
   },
 })
