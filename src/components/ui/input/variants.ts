c5bb5d444bc11291dbd1f0887e1883f0c05170a6
zweird <zweir.dd@gmail.com>
2025-10-20 12:01:24 +0200

    refactor(input): change placeholder text color

--- src/components/ui/input/variants.ts
+++ src/components/ui/input/variants.ts
@@ -5,6 +5,7 @@ export const input = tv({
     "flex h-9 w-full bg-transparent px-2 py-1",
     "border-input border",
     "focused:ring focused:outline-none focused:ring-ring focused:border-ring",
+    "placeholder:text-muted-foreground/70",
     "transition-colors",
     "disabled:opacity-disabled disabled:cursor-not-allowed",
   ],
