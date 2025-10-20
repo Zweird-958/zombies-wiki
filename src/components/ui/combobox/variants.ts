f8f5b36591d908ba55bb2aebe75b884e1cb38ec1
zweird <zweir.dd@gmail.com>
2025-10-20 16:52:54 +0200

    refactor(combobox): use border color

--- src/components/ui/combobox/variants.ts
+++ src/components/ui/combobox/variants.ts
@@ -11,16 +11,17 @@ export const combobox = tv({
     icon: "justifty-center absolute top-0 right-2 flex h-full items-center",
     input:
       "border-input w-full rounded-md border px-2 py-1 outline-0 focus:ring-2",
-    separator: "bg-popover-foreground my-2 rounded-md border-[0.1px]",
+    separator: "border-border my-2 rounded-md border-[0.1px]",
     popup: [
-      "bg-popover text-popover-foreground box-border scroll-py-2 overflow-y-auto overscroll-contain rounded-md px-2 py-2",
+      "border-border border",
+      "bg-popover text-popover-foreground scroll-py-2 overflow-y-auto overscroll-contain rounded-md px-2 py-2",
       "max-h-[min(var(--available-height),23rem)] w-[var(--anchor-width)] max-w-[var(--available-width)] origin-[var(--transform-origin)]",
       "starting-style:scale-95 starting-style:opacity-0 ending-style:scale-95 ending-style:opacity-0 transition duration-100 ease-in-out",
       "[&[data-empty]_[data-slot=empty]]:py-2",
     ],
     item: [
       "highlighted:bg-accent highlighted:text-accent-foreground",
-      "text-muted-foreground svg-not-size:size-4 box-border flex cursor-pointer items-center gap-2 rounded-md p-2 outline-none select-none",
+      "text-muted-foreground svg-not-size:size-4 flex cursor-pointer items-center gap-2 rounded-md p-2 outline-none select-none",
     ],
     empty: "cursor-not-allowed px-2 select-none",
     groupLabel: "px-0.5 py-2",
