cbaae84963a7f4d19b57d34fc2e7b4224417a804
zweird <zweir.dd@gmail.com>
2025-10-20 11:39:16 +0200

    refactor(input): add height

--- src/components/ui/input/variants.ts
+++ src/components/ui/input/variants.ts
@@ -2,7 +2,8 @@ import { tv } from "tailwind-variants"
 
 export const input = tv({
   base: [
-    "border-input rounded-md border bg-transparent px-2 py-1",
+    "flex h-9 w-full rounded-md bg-transparent px-2 py-1",
+    "border-input border",
     "focused:ring focused:outline-none focused:ring-ring focused:border-ring",
     "transition-colors",
     "disabled:opacity-disabled disabled:cursor-not-allowed",
