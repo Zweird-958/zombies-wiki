afc97dfe24398179e2be3ae6fe9b165c4ebe8567
zweird <zweir.dd@gmail.com>
2025-10-20 11:15:05 +0200

    chore(prettier): add tailwind variant

--- src/components/ui/dialog/variants.ts
+++ src/components/ui/dialog/variants.ts
@@ -4,17 +4,17 @@ export const dialog = tv({
   slots: {
     backdrop: [
       "starting-style:opacity-0 ending-style:opacity-0 transition-all duration-150",
-      "fixed inset-0 bg-black opacity-70 ",
+      "fixed inset-0 bg-black opacity-70",
     ],
     popup: [
       "bg-popover text-popover-foreground border-border max-w-[calc(100vw-3rem)] min-w-96 rounded-lg border p-6",
-      "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50",
+      "fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2",
       "starting-style:scale-90 starting-style:opacity-0 ending-style:scale-90 ending-style:opacity-0 transition-all duration-150",
     ],
     close: [
       "absolute top-4 right-4 rounded-full opacity-70 outline-0 transition-opacity hover:opacity-100",
       "svg-not-size:size-4 svg:pointer-events-none svg:shrink-0",
-      " disabled:pointer-events-none",
+      "disabled:pointer-events-none",
     ],
     content: "pt-2",
     title: "text-lg font-medium",
--- src/components/ui/label/variants.ts
+++ src/components/ui/label/variants.ts
@@ -3,7 +3,7 @@ import { tv } from "tailwind-variants"
 export const label = tv({
   base: [
     "text-primary block leading-none font-medium transition-opacity select-none",
-    "peer-disabled:cursor-not-allowed peer-disabled:opacity-disabled",
+    "peer-disabled:opacity-disabled peer-disabled:cursor-not-allowed",
     "has-[+input:is(:disabled)]:opacity-disabled has-[+input:is(:disabled)]:cursor-not-allowed",
   ],
 })
