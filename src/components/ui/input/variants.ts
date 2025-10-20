2a37184e786e49083fcd1808998198c25b81679a
zweird <zweir.dd@gmail.com>
2025-10-20 11:25:50 +0200

    refactor(input): add ring color

--- src/components/ui/input/variants.ts
+++ src/components/ui/input/variants.ts
@@ -3,7 +3,8 @@ import { tv } from "tailwind-variants"
 export const input = tv({
   base: [
     "border-input rounded-md border px-2 py-1",
-    "focused:ring focused:outline-none",
+    "focused:ring focused:outline-none focused:ring-ring focused:border-ring",
+    "transition-colors",
     "disabled:opacity-disabled disabled:cursor-not-allowed",
   ],
 })
