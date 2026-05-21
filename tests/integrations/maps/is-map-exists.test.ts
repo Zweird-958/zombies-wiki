d016e819d5e04f5a772f0224bbfefc2587072444
zweird <zweir.dd@gmail.com>
2026-05-21 16:13:36 +0200

    fix(tests): duplicate name on generate

--- tests/integrations/maps/is-map-exists.test.ts
+++ tests/integrations/maps/is-map-exists.test.ts
@@ -1,54 +1,34 @@
 import { faker } from "@faker-js/faker"
+import { generateGame } from "@tests/utils/generate-game"
+import { generateMap } from "@tests/utils/generate-map"
 import { beforeAll, describe, expect, it } from "vitest"
 
 import { isMapExists } from "@/api/utils/maps/is-map-exists"
-import { slugify } from "@/api/utils/slugify"
 import { db } from "@/db"
-import { games, images, maps } from "@/db/schemas"
-
-const name = faker.word.words({ count: { min: 1, max: 5 } })
-
-const gameData: Omit<typeof games.$inferInsert, "imageId"> = {
-  name,
-  slug: slugify(name),
-  releaseYear: faker.number.int({ min: 1970, max: 2050 }),
-}
-
-const mapData: Omit<typeof maps.$inferInsert, "imageId" | "gameId"> = {
-  name: faker.word.words({ count: { min: 1, max: 5 } }),
-  slug: "",
-}
 
 let gameId = ""
+let mapSlug = ""
 
 beforeAll(async () => {
-  const [image] = await db
-    .insert(images)
-    .values({ url: faker.image.url() })
-    .returning()
-
-  const [game] = await db
-    .insert(games)
-    .values({ ...gameData, imageId: image.id })
-    .returning({ id: games.id })
+  const game = await generateGame()
 
   gameId = game.id
 
-  await db
-    .insert(maps)
-    .values({ ...mapData, imageId: image.id, gameId: game.id })
+  const map = await generateMap({ gameId })
+
+  mapSlug = map.slug
 })
 
 describe("isMapExists", () => {
   it("should return true if the map exists", async () => {
-    const isExists = await isMapExists({ slug: mapData.slug, gameId }, db)
+    const isExists = await isMapExists({ slug: mapSlug, gameId }, db)
 
     expect(isExists).toBe(true)
   })
 
   it("should return false if the map exists but for another game", async () => {
     const isExists = await isMapExists(
-      { slug: mapData.slug, gameId: faker.string.uuid() },
+      { slug: mapSlug, gameId: faker.string.uuid() },
       db,
     )
 
