65fa31338ba2c99b015a2c0f9a5483ab894c6097
zweird <zweir.dd@gmail.com>
2026-05-22 09:55:48 +0200

    fix: ci

--- src/app/(public)/[game]/[map]/page.tsx
+++ src/app/(public)/[game]/[map]/page.tsx
@@ -1,10 +1,6 @@
-import { getTranslations } from "next-intl/server"
 import { notFound } from "next/navigation"
 
 import { client } from "@/api/client"
-import GuideCard from "@/components/guides/guide-card"
-import ItemsLayout from "@/components/items/items-layout"
-import DeleteMap from "@/components/maps/delete-map"
 import MapPage from "@/components/maps/map-page"
 
 type Props = {
@@ -12,8 +8,7 @@ type Props = {
 }
 
 const Page = async ({ params }: Props) => {
-  const { map: mapName, game } = await params
-  const t = await getTranslations("maps")
+  const { map: mapName } = await params
   const data = await (
     await client.maps[":slug"].$get({ param: { slug: mapName } })
   ).json()
